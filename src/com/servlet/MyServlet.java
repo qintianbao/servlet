b774984d419926fea463cc80bb4203d0271209fb
qtb <763145819@qq.com>
2019-06-04 19:38:22 +0800

    frist commit

---
+++ src/com/servlet/MyServlet.java
@@ -0,0 +1,41 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet{
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init....");
+		String value = super.getInitParameter("thisname");
+		System.out.println("当前容器初始化值："+value);
+		ServletContext servlet = super.getServletContext();
+		String global = servlet.getInitParameter("globalname");
+		System.out.println("web容器初始化值："+global);
+		
+		String string = new String();
+	}
+	@Override
+	public void destroy() {
+		System.out.println("destroy...");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("this doGet...");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("this doPost...");
+		this.doGet(req, resp);
+	}
+	
+}
---
+++ src/com/servlet/MyServlet2.java
@@ -0,0 +1,33 @@
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet2
+ */
+public class MyServlet2 extends HttpServlet {
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init servlet2...");
+		
+	}
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("doGet...");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		System.out.println("doPost。。。");
+	}
+
+}
